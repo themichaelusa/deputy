08af9d66aee0b6aabc9d02a01d484c24492caa20
Michael Usachenko <meu2@illinois.edu>
2020-02-15 15:47:14 -0600

    deputy run now works

--- deputy.sh
+++ deputy.sh
@@ -5,14 +5,6 @@
 
 # DEPUTY CLI FUNCS
 
-# deputy addvm <vm_name>
-function deputy_addvm () {
-	vm_str='"'
-	vm_str+=$1
-	vm_str+='"'
-	VBoxManage modifyvm vm_str --natpf1 "ssh,tcp,,3022,,22" 
-}
-
 # deputy register <vm_name> <uname> <proj_name>
 function deputy_register () {
 	cd /usr/local/share/deputy_cli
@@ -24,40 +16,52 @@ function deputy_register () {
 	vm_name_str+=$1
 	uname_str='uname='
 	uname_str+=$2
-	#proj_name_str='proj_name='
-	#proj_name_str+=$3
 
 	vm_name_str >> md.txt
 	uname_str >> md.txt
-
-	#$hashed >> $1
-	#$hashed >> $2
-	#$hashed >> $3
 	mkdir $3
 
-	deputy_addvm $1
+	# modify VM to work with ssh + 
+	VBoxManage modifyvm $1 --natpf1 "ssh,tcp,,3022,,22"
+	echo -e "\033[1;93m[PROMPT] ENTER $1 VM PASSWORD: \033[0m"
+	ssh-copy-id -p 3022 $2@127.0.0.1
+	echo -e "\033[1;93m[READY] DEPUTY TUNNEL UTIL FOR: $1\033[0m"
 }
 
 function deputy_connect () {
-	vm_str='"'
-	vm_str+=$1
-	vm_str+='"'
-	VBoxManage startvm $(vm_str) --type headless
-	echo -e "\033[1mConnecting to $1...\033[0m"
+	echo -e "\033[1;93m[INIT] DEPUTY TUNNEL TO: $1\033[0m"
+	VBoxManage startvm $1 --type headless
 	sleep 2
-	ssh -p 3022 mikeusa@127.0.0.1
-	export deputy_vm_context=vm_str
+	echo -e "\033[1;32m[READY] DEPUTY TUNNEL TO: $1 \033[0m"
 }
 
-#function deputy_exit () {;}
+function deputy_run () {
+	cdir=${PWD##*/}
+	echo "TEST CDIR: $cdir"
+	dep_root="/home/mikeusa/Desktop/deputy"
+	scp -P 3022 -r $PWD mikeusa@127.0.0.1:$dep_root
+	ssh -p 3022 mikeusa@127.0.0.1 cd $dep_root && cd $cdir; $@
+}
 
+function deputy_exit () {	
+	echo -e "\033[1;93m[INIT] KILL DEPUTY TUNNEL: $1\033[0m"
+	VBoxManage controlvm $1 poweroff soft
+	echo -e "\033[1;31m[KILLED] DEPUTY TUNNEL: $1\033[0m"
+}
+
+#echo "COMMAND: $1"
 # deputy main logic 
-if [ $1=="addvm" ]; then
-	deputy_addvm $2
-elif [ $1=="register" ]; then
-	deputy_login $2 $3 $4 
-elif [ $1=="connect" ]; then
+CMD=$1
+shift
+
+if [ $CMD == "register" ]; then
+	deputy_register $1 $2 $3 
+elif [ $CMD == "connect" ]; then
 	deputy_connect $1
+elif [ $CMD == "exit" ]; then
+	deputy_exit $1
+elif [ $CMD == "run" ]; then
+	deputy_run $@
 fi
 
 
